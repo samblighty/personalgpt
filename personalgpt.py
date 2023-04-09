36d4f06819ba94e42dfbf2b448ddda58a110c920
Sameer Ahmad <sam.ahmad.mc@gmail.com>
2023-04-09 01:42:13 +0100

    Version 1 of the system

--- personalgpt.py
+++ personalgpt.py
@@ -3,6 +3,7 @@ import os
 import openai
 import pinecone
 from colorama import init, Fore, Style
+import time
 
 init()  # Init Colorama
 load_dotenv()  # Init ENV vars
@@ -11,12 +12,58 @@ GPT_API_KEY = os.getenv("GPT_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
 
-DIMENSION = 1024
+PINECONE_ENV = "us-east1-gcp"
+DIMENSION = 1536
 METRIC = "cosine"
 POD_TYPE = "p2"
 GPT_MODEL = "gpt-3.5-turbo"
 GPT_TEMPERATURE = 0.8
 EMBEDDING_MODEL = "text-embedding-ada-002"
+TOP_RESULTS = 5
+
+
+class Gpt:
+    def __init__(self):
+        openai.api_key = GPT_API_KEY
+
+    def chat(self, message):
+        additionalInput = self.getAdditionalInfo(message)
+        message += additionalInput
+        printSys(f"Additional information passed: {additionalInput}")
+        response = openai.ChatCompletion.create(
+            model=GPT_MODEL,
+            messages=[
+                {
+                    "role": "user",
+                    "content": message,
+                }
+            ],
+            temperature=GPT_TEMPERATURE,
+        )
+        gptResponse = response.choices[0].message.content.strip()
+        printChat(gptResponse)
+        return gptResponse
+
+    def getEmbedding(self, text):
+        text = text.replace("\n", " ")
+        return openai.Embedding.create(input=[text], model=EMBEDDING_MODEL)["data"][0][
+            "embedding"
+        ]
+
+    def getAdditionalInfo(self, message):
+        additionalInfo = vectorSearch(message)
+        additionalString = (
+            f"\nThis list of information may be relevant in answering the prompt: \n"
+        )
+        for info in additionalInfo:
+            additionalString += f"{info}\n"
+        return additionalString
+
+
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+db = pinecone.Index(PINECONE_INDEX_NAME)
+gpt = Gpt()
+
 
 if PINECONE_INDEX_NAME not in pinecone.list_indexes():
     pinecone.create_index(
@@ -32,41 +79,22 @@ def printChat(msg):
     print(f"{Fore.CYAN}GPT:{Style.RESET_ALL} {msg}")
 
 
-class Gpt:
-    def __init__(self):
-        openai.api_key = GPT_API_KEY
-
-    def chat(self, message):
-        finalMessage = message + self.getAdditionalInfo()
-        printSys(f"Asking GPT about: {finalMessage}")
-        response = openai.ChatCompletion.create(
-            model=GPT_MODEL,
-            messages=[
-                {
-                    "role": "user",
-                    "content": finalMessage,
-                }
-            ],
-            temperature=GPT_TEMPERATURE,
-        )
-        gptResponse = response.choices[0].message.content.strip()
-        printChat(gptResponse)
-        return gptResponse
-
-    def getEmbedding(self, text):
-        text = text.replace("\n", " ")
-        return openai.Embedding.create(input=[text], model=EMBEDDING_MODEL)["data"][0][
-            "embedding"
-        ]
-
-    def getAdditionalInfo(self):
-        return " "
+def vectorUpload(data):
+    vector = gpt.getEmbedding(data)
+    db.upsert([(f"gpt_{time.time()}", vector, {"prompt": data})])
 
 
-db = pinecone.Index(PINECONE_INDEX_NAME)
-gpt = Gpt()
+def vectorSearch(data):
+    vector = gpt.getEmbedding(data)
+    results = db.query(vector, top_k=TOP_RESULTS, include_metadata=True)
+    topResults = sorted(results.matches, key=lambda x: x.score, reverse=True)
+    return [(str(item.metadata["prompt"])) for item in topResults]
+
 
 while True:
     printSys("Please enter a prompt")
     prompt = input().strip()
+
     gpt.chat(prompt)
+
+    vectorUpload(prompt)
